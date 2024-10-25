684e2da04a3216c63c5af8d45f8e8fee6047e92b
Family Dudkevych <arkadii@Mac-mini-Family.local>
2024-10-25 15:01:03 +0300

    test2

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-alert('hello world'); 
-alert('hello')
+let lesonNumber = 1;
+let homeWork = 81;
+let myhomeWorks = 2;
+let thusday = 12;
+
+alert('today leson number' + lesonNumber + myhomeWorks);
+alert('home work' + homeWork + lesonNumber + lesonNumber);
+console.log('thusday' + homeWork + thusday+lesonNumber +myhomeWorks);
